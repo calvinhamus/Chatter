51744615ea1be0c6a40ab50fb061cb2fe666d27d
calvinhamus <calvinhamus@u.boisestate.edu>
2014-10-28 19:53:03 -0600

    initial

---
+++ Chatterer/target/Chatterer/index.jsp
@@ -0,0 +1,61 @@
+<HTML>
+<HEAD>
+<TITLE>Chatter</TITLE>
+<script src="scripts/script.js"></script>
+</HEAD>
+<BODY BGCOLOR="FFFFFF">
+<div id="597-like"></div>
+<H1>Chatter Alpha</H1>
+<p>Response Status:</p><b><h3><p id="status"></p></h3></b>
+<div>
+	Create or Get User: <input type="text" id="userName"><br>
+	<button type="button" onclick="createUser()">Create</button>
+	<button type="button" onclick="getUserByName()">Get User By Name</button>
+	<p>Created Id:</p><p id="createdId"></p>
+	<p>User Id From Name </p><b><p id="userId"></p></b>
+	Login  Id: <input type="text" id="login"><br>
+	<button type="button" onclick="login()">Login</button>
+	
+
+</div>
+<div>
+	<button type="button" onclick="getPendingIncoming()">Get Pending Incoming</button>
+	<button type="button" onclick="getPendingOutGoing()">Get Pending Outgoing</button>
+	<p>Pending Incoming Results: </p><b><p id="pendingIncomingResults"></p></b>
+	<p>Pending Outgoing Results:</p><b><p id="pendingOutgoingResults"></p></b>
+</div>
+<div>
+	Follow User: <input type="text" id="userToFollow"><br>
+	<button type="button" onclick="followUser()">Follow User</button>
+	<button type="button" onclick="unfollowUser()">Unfollow User</button>
+	<button type="button" onclick="listFollowering()">List Following</button>
+	<button type="button" onclick="listFollowers()">List Followers</button>
+	<p>Followed User ID: </p><b><p id="followedUserID"></p></b>
+	<p>UnFollowed User ID: </p><b><p id="unfollowedUserID"></p></b>
+	<p>Following List: </p><b><p id="followingList"></p></b>
+	<p>Followers List: </p><b><p id="followersList"></p></b>
+</div>
+<div>
+	Tweet Msg: <input type="text" id="tweetMsg"><br>
+	<button type="button" onclick="tweet()">Send Tweet</button>
+	<p>Tweet Id Result: </p><b><p id="tweetId"></p></b>
+</div>
+<div>
+	Get Tweet By Id: <input type="text" id="tweetById"><br>
+	<button type="button" onclick="getTweetById()">Get Tweet</button>
+	<p>Pending Incoming Results: </p><b><p id="tweetByIdResult"></p></b>
+</div>
+<div>
+	Destroy Tweet: <input type="text" id="tweetToRemove"><br>
+	<button type="button" onclick="removeTweet()">Remove Tweet</button>
+	<p>Remove Tweet Id: </p><b><p id="removedTweetId"></p></b>
+</div>
+<div>
+	Retweet Tweet: <input type="text" id="reTweet"><br>
+	<button type="button" onclick="retweetTweet()">Retweet Tweet</button>
+	<p>Retweet Result: </p><b><p id="retweetResult"></p></b>
+</div>
+
+<hr/>
+</BODY>
+</HTML> 
---
+++ Chatterer/target/Chatterer/scripts/script.js
@@ -0,0 +1,297 @@
+var BASE_URL = "http://cs597-calvin-loadbalancer:80/socialnetwork/chatter/";
+//var BASE_URL = "http://localhost:8080/socialnetwork/chatter/";
+
+onload = function() {
+    	var url = document.URL;
+		
+};
+
+function getUserByName()
+{
+//TODO getUserId function
+	var userName =  document.getElementById("userName").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'users/'+userName , false);
+    		//alert(BASE_URL + 'users/'+userName);
+    		request.onload = function() {
+    		//alert(request.status);
+        		if (request.status === 201) {
+            			document.getElementById("userId").innerHTML = request.responseText;
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		request.send();
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function login()
+{
+	var userName =  document.getElementById("login").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'users/login/'+userName , false);
+    		request.onload = function() {
+    		alert(request.status);
+        		if (request.status === 202) {
+            			document.getElementById("status").innerHTML = request.status;
+						//document.getElementById("status").innerHTML = request.status;
+        			} else {
+            				document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function createUser()
+{
+	var userName =  document.getElementById("userName").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'users/create/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+						document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("createdId").innerHTML ="New Id: "+ request.responseText;//Update result
+        			} else {
+            				document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function getPendingIncoming()
+{
+	//var userName =  document.getElementById("userName").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'friendships/incoming' , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("pendingIncomingResults").innerHTML = "Pending Incoming: "+request.responseText;//Update result
+        			} else {
+            				document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function getPendingOutGoing()
+{
+	//var userName =  document.getElementById("userName").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'friendships/incoming' , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("pendingOutgoingResults").innerHTML =  "Pending Outgoing: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function followUser()
+{
+	var userName =  document.getElementById("userToFollow").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'friendships/create/'+userName , false);
+    		request.onload = function() {
+			alert(request.status);
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("followedUserID").innerHTML =  "Friends Id: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function unfollowUser()
+{
+	var userName =  document.getElementById("userToFollow").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'friendships/destroy/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("unfollowedUserID").innerHTML =  "Unfriended Id: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function listFollowers()
+{
+	//var userName =  document.getElementById("userToFollow").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'friendships/followers/list' , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("followersList").innerHTML =  "Followers List: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function listFollowering()
+{
+	//var userName =  document.getElementById("userToFollow").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'friendships/friends/list' , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("followingList").innerHTML =  "Following List: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function tweet()
+{
+	var userName =  document.getElementById("tweetMsg").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'tweet/tweet/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("tweetId").innerHTML =  "Tweet Id "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function getTweetById()
+{
+	var userName =  document.getElementById("tweetById").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("GET", BASE_URL + 'tweet/show/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("tweetByIdResult").innerHTML =  "Tweet: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function removeTweet()
+{
+	var userName =  document.getElementById("tweetToRemove").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'tweet/destroy/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("removedTweetId").innerHTML =  "Deleted Tweet Id "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
+function retweetTweet()
+{
+	var userName =  document.getElementById("reTweet").value;
+	try{
+		var request = new XMLHttpRequest();
+    		request.open("POST", BASE_URL + 'tweet/retweet/'+userName , false);
+    		request.onload = function() {
+        		if (request.status === 201) {
+            			document.getElementById("status").innerHTML = request.status;
+            			document.getElementById("retweetResult").innerHTML =  "Retweet: "+request.responseText;//Update result
+        			} else {
+            			document.getElementById("status").innerHTML = 'Error '+request.status;
+        			}
+    			};
+				document.getElementById("status").innerHTML += "Status: " + request.status;
+    		//request.setRequestHeader("Content-Type", 'text/plain');
+    		request.send(document.URL);
+    }catch(err){
+    			alert(err);
+            document.getElementById("status").innerHTML += "\nXMLHttprequest error: " + err.description;
+    }
+}
